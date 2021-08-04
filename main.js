c194e0e688e8bee4a129e0a9965fe02135e6a9e1
Aleks <aleksandar.trpkovski@gmail.com>
2021-08-04 19:03:46 +1000

    on small screens now there is no animation on moto and bg_img

--- main.js
+++ main.js
@@ -9,24 +9,21 @@ let bgPositionX =
     ? "70%"
     : "100%";
 let showMoreBtnHeight = document.getElementById("btn-show-more").offsetHeight;
+let isMobileScreen = getWidthSize() <= 767;
 const windowSize = setWindowSize();
 
-// Watch WIDTH property on value change
-windowSize.setWatcherWidth((val) => {
-  console.log("Width: ", val);
-});
+// GSAP Initialization. We add animations to the timeline below
+let tl = gsap.timeline();
 
-// Watch HEIGHT property on value change
-windowSize.setWatcherHeight((val) => {
-  console.log("Height: ", val);
+// Watch WIDTH property on value change
+windowSize.setWatcherWidth(() => {
+  clearAnimation();
+  updateValues();
 });
 
 // Event Listeners
 btnShowMore.addEventListener("click", showMore);
 window.addEventListener("resize", setWindowResize);
-
-// GSAP Initialization. We add animations to the timeline below
-let tl = gsap.timeline();
 
 async function showMore() {
   // Check if the timeline is Active. If there is an existing animation, return. Otherwise continue.
@@ -41,15 +38,17 @@ function showMoreAnimation() {
   return new Promise((resolve) => {
     tl = gsap.timeline({ onComplete: () => resolve() });
     tl.to("#show-more-arrow", { duration: 0.5, bottom: 0, rotation: 180 });
-    tl.to(
-      "#moto",
-      {
-        duration: 0.5,
-        ease: "power4.out",
-        opacity: 0,
-      },
-      "-=0.5"
-    );
+    if (!isMobileScreen) {
+      tl.to(
+        "#moto",
+        {
+          duration: 0.5,
+          ease: "power4.out",
+          opacity: 0,
+        },
+        "-=0.5"
+      );
+    }
     tl.to("#btn-show-more", {
       duration: 0.3,
       ease: "power4.out",
@@ -61,26 +60,28 @@ function showMoreAnimation() {
       bottom: 0,
       ease: "power4.out",
     });
-    tl.to(
-      "#bg_img",
-      {
-        delay: 0.1,
-        duration: 0.3,
-        backgroundPosition: `${bgPositionX} -${footerHeight}px`,
-        ease: "power4.out",
-      },
-      "-=0.4"
-    );
-    tl.to(
-      "#subscribe-group",
-      {
-        delay: 0.1,
-        duration: 0.3,
-        ease: "power4.out",
-        y: -250,
-      },
-      "-=0.4"
-    );
+    if (!isMobileScreen) {
+      tl.to(
+        "#bg_img",
+        {
+          delay: 0.1,
+          duration: 0.3,
+          backgroundPosition: `${bgPositionX} -${footerHeight}px`,
+          ease: "power4.out",
+        },
+        "-=0.4"
+      );
+      tl.to(
+        "#subscribe-group",
+        {
+          delay: 0.1,
+          duration: 0.3,
+          ease: "power4.out",
+          y: -250,
+        },
+        "-=0.4"
+      );
+    }
   });
 }
 
@@ -95,6 +96,29 @@ function getWidthSize() {
 
 function getHeightSize() {
   return document.documentElement.clientHeight;
+}
+
+function updateValues() {
+  btnShowMore = document.getElementById("btn-show-more");
+  footerHeight = document.getElementById("footer").offsetHeight;
+  bgPositionX =
+    getWidthSize() <= 1919 && getWidthSize() > 1469
+      ? "50%"
+      : getWidthSize() <= 1469
+      ? "70%"
+      : "100%";
+  showMoreBtnHeight = document.getElementById("btn-show-more").offsetHeight;
+  isMobileScreen = getWidthSize() <= 767;
+}
+
+function clearAnimation() {
+  tl.clear();
+  gsap.set("#show-more-arrow", { clearProps: "all" });
+  gsap.set("#moto", { clearProps: "all" });
+  gsap.set("#btn-show-more", { clearProps: "all" });
+  gsap.set("#footer", { clearProps: "all" });
+  gsap.set("#bg_img", { clearProps: "all" });
+  gsap.set("#subscribe-group", { clearProps: "all" });
 }
 
 function setWindowSize() {
